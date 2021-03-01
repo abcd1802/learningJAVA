9e822734e622a1a01f3ed3fdf47235edd25b8f0e
SAMSUNG <SAMSUNG@SAMSUNG-PC>
2021-03-01 16:06:47 -0500

    Add a new message "This is my first JAVA program!"

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("This is Summer");
+		System.out.println("This is my first JAVA program!");
 	}
 }
